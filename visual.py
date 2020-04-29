ffc4f3918db797171085a2b5a4e3515046f133e6
doctorblinch <docblinch@gmail.com>
2020-04-29 20:19:26 +0300

    working on front

--- visual.py
+++ visual.py
@@ -1,15 +1,33 @@
 import streamlit as st
+import os
 
 
 def presentation_page():
     st.markdown(markdown2string('data/markdown/presentation_page.md'))
 
 
+def file_selector(folder_path='./data/input_files'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+
 def solution_page():
     st.title('Сторінка з рішенням задачі')
     input_type = st.selectbox('Оберіть спосіб вхідних даних', ['File', 'Data Base', 'Random'])
-    if st.button('Ok') and input_type == 'Random':
-        st.balloons()
+    if st.button('Обрати'):
+        if input_type == 'Random':
+            st.button('Solve')
+
+        if input_type == 'Data Base':
+            st.dataframe([1,23,4])
+            st.number_input('Input ID')
+            st.button('Solve')
+
+        if input_type == 'File':
+            filename = file_selector()
+            st.write('Ви обрали `%s`' % filename)
+            st.button('Solve')
 
 
 def markdown2string(file_path):
