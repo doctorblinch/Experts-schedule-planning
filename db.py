80c4b8797ccf2ae3928fe144036ac72e357860c2
doctorblinch <docblinch@gmail.com>
2020-05-01 11:25:19 +0300

    typo in pres page, DB now init only when need so it can work without internet + can write tasks with solutions to db, visual works with all 3 input methods

--- db.py
+++ db.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import sessionmaker, relationship
 
 import os
 
-db_url = os.getenv("db_url")
-
+db_inited = False
 Base = declarative_base()
 
 
@@ -13,7 +12,7 @@ class Task(Base):
     __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, default='Task #{}'.format(id))
+    name = Column(String, default='')
 
     solutions = relationship("Solution")  # one to many
     intermediate_results = relationship("IntermediateResult")  # one to many
@@ -41,6 +40,16 @@ class IntermediateResult(Base):
     task_id = Column(Integer, ForeignKey('tasks.id'))
 
 
+def init_db():
+    db_url = os.getenv("db_url")
+    engine = create_engine(db_url)
+    Base.metadata.create_all(bind=engine)
+    global Session
+    Session = sessionmaker(bind=engine)
+    global db_inited
+    db_inited = True
+
+
 class TaskCondition(Base):
     __tablename__ = 'tasks_conditions'
 
@@ -52,12 +61,28 @@ class TaskCondition(Base):
     task_id = Column(Integer, ForeignKey('tasks.id'))
 
 
-engine = create_engine(db_url)
-Base.metadata.create_all(bind=engine)
-Session = sessionmaker(bind=engine)
+def write_task_to_db(condition, answer, target_func_value, method=''):
+    if not db_inited:
+        init_db()
+    B, E = [], []
+    for i in condition:
+        B.append(i[0])
+        E.append(i[1])
+
+    session = Session()
+    task_condition = TaskCondition(B=B, E=E)
+    solution = Solution(X=answer, metrics=target_func_value, method=method)
+    task = Task(solutions=[solution], condition=task_condition)
+    session.add(task)
+    session.add(task_condition)
+    session.add(solution)
+    session.commit()
+    session.close()
 
 
 def get_presets_conditions():
+    if not db_inited:
+        init_db()
     session = Session()
     all_tasks = session.query(Task).all()
     conditions_ids = [task.condition.id for task in all_tasks]
@@ -82,4 +107,3 @@ def get_presets_conditions():
 # session.commit()
 # print([(i.id, i.name) for i in session.query(Task).all()])
 #
-
--- visual.py
+++ visual.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import SessionState
+import random
 import os
 
-from db import get_presets_conditions
+from db import get_presets_conditions, write_task_to_db
 from algorithms import ExpertsTask
 
 
@@ -16,13 +17,35 @@ def file_selector(folder_path='./data/input_files'):
     return os.path.join(folder_path, selected_filename)
 
 
-def show_answer(condition):
+def show_answer(condition, write_to_db=True):
     st.balloons()
     st.title('Відповідь')
     task = ExpertsTask(condition)
     task.dynamic_algorithm()
     st.write(task.tf_res)
     st.write(task.experts_res_list)
+    if write_to_db:
+        write_task_to_db(condition, task.experts_res_list, task.tf_res, method=task.solution_method)
+
+
+def generate_random_condition(quantity, min_val, max_val, distribution):
+    condition = []
+    if distribution == 'Нормальний':
+        for _ in range(quantity):
+            a = int(random.normalvariate((max_val + min_val) / 2, (max_val + min_val) / 5))
+            b = int(random.normalvariate((max_val + min_val) / 2, (max_val + min_val) / 5))
+            a = max_val if a > max_val else a
+            a = min_val if a < min_val else a
+            b = max_val if b > max_val else b
+            b = min_val if b < min_val else b
+            condition.append((min(a, b), max(a, b)))
+    elif distribution == 'Рівномірний':
+        for _ in range(quantity):
+            a = random.randint(min_val, max_val)
+            b = random.randint(min_val, max_val)
+            condition.append((min(a, b), max(a, b)))
+
+    return condition
 
 
 def solution_page():
@@ -32,21 +55,25 @@ def solution_page():
     session_state.input_type = st.selectbox('Оберіть спосіб вхідних даних', ['File', 'Data Base', 'Random'])
 
     if session_state.input_type == 'Random':
-            quantity = st.number_input('Кількість експертів', step=1, value=5, min_value=1, max_value=50)
-            min_val = st.number_input('Мінімальне значеня', step=1, value=1, min_value=1, max_value=999)
-            max_val = st.number_input('Максимальне значеня', step=1, value=40, min_value=1, max_value=999)
-            distribution = st.selectbox('Оберіть розподіл випадкових велечин', ['Рівномірне', 'Нормальне'])
+        quantity = st.number_input('Кількість експертів', step=1, value=5, min_value=1, max_value=50)
+        min_val = st.number_input('Мінімальне значеня', step=1, value=1, min_value=1, max_value=999)
+        max_val = st.number_input('Максимальне значеня', step=1, value=40, min_value=1, max_value=999)
+        distribution = st.selectbox('Оберіть розподіл випадкових велечин', ['Рівномірний', 'Нормальний'])
 
-            if st.button('Solve'):
-                show_answer([])
-        # кол-во, значения от до, и распределния
+        if st.button('Розв\'язати'):
+            condition = generate_random_condition(quantity, min_val, max_val, distribution)
+            st.write('Згенерували наступну умову: {}'.format(condition))
+            show_answer(condition)
+    # кол-во, значения от до, и распределния
 
     if session_state.input_type == 'Data Base':
         conditions = get_presets_conditions()
         st.dataframe(conditions, width=1000)
-        st.number_input('Input ID', step=1, value=1, min_value=1, max_value=len(conditions))
-        if st.button('Solve'):
-            show_answer(conditions)
+        condition_id2solve = st.number_input('Введіть ID', step=1, value=1, min_value=1, max_value=len(conditions))
+
+        condition2solve = list(filter(lambda cond: cond.get('task_id') == condition_id2solve, conditions))[0]
+        if st.button('Розв\'язати'):
+            show_answer(condition2solve.get('experts', []))
 
         # метод решения
 
@@ -55,7 +82,7 @@ def solution_page():
         st.write('Ви обрали `%s`' % filename)
         condition = parse_condition_csv(filename)
         st.write(condition)
-        if st.button('Solve'):
+        if st.button('Розв\'язати'):
             show_answer(condition)
 
 
@@ -101,5 +128,6 @@ def main():
 
     if page == 'Technical details':
         technical_page()
+
 
 main()
